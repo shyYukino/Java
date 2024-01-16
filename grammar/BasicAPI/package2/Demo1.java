8174fd1e999317f6daaf0b2e1edc81b202329b8f
YUKIPEDIA <123065412+shyYukino@users.noreply.github.com>
2024-01-16 14:48:32 +0800

    Add files via upload

---
+++ grammar/BasicAPI/package2/Demo1.java
@@ -0,0 +1,7 @@
+package grammar.BasicAPI.package2;
+
+public class Demo1 {
+    public void print() {
+        System.out.println("Hello World2");
+    }
+}
---
+++ grammar/BasicAPI/package2/Demo2.java
@@ -0,0 +1,7 @@
+package grammar.BasicAPI.package2;
+
+public class Demo2 {
+    public void print() {
+        System.out.println("Package2");
+    }
+}
